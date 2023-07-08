3ebc797c875c2da5b2f1900c2bc095fa9000b6ab
jenna <je.enna.a@hotmail.com>
2023-07-08 16:14:35 +0200

    hook

--- wp-content/themes/theme enfant/functions.php
+++ wp-content/themes/theme enfant/functions.php
@@ -1,8 +1,33 @@
-<?php
-
-// Action qui permet de charger des scripts dans notre thème
-add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-function theme_enqueue_styles(){
-    // Chargement du style.css du thème parent Twenty Twenty
+<?php add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+function theme_enqueue_styles()
+{
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+   
 }
+
+
+add_filter( 'wp_nav_menu_items', 'ajout_admin_menu', 10, 2 );
+
+function ajout_admin_menu ( $menu_items, $args ) {
+   
+   
+      if (is_user_logged_in()) {
+         $menu_items .= '<li class="menu-item menu-item-type-post_type menu-item-object-page parent hfe-creative-menu"><a class="hfe-menu-item " href="http://localhost/PLANTY/admin/"> Admin  </a></li>';
+      }
+   
+
+   return $menu_items;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
